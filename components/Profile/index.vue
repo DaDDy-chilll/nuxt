5735a9369ae78208ac05b468d8b9de86629a16af
Who <blacklistuser420@gmail.com>
2024-11-16 23:33:11 +0630

    updated

---
+++ components/Profile/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Profile</h1>
+    <ProfileHeader />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/events/[id].vue
@@ -0,0 +1,7 @@
+<script setup>
+const nuxt = useNuxtApp();
+
+</script>
+<template>
+  <h1>Events id{{ $route.params.id }}</h1>
+</template>
